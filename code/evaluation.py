1633cec1ebcde2a26fe3978300eb06aae9bece3a
Alessandro Pina <pina.alessandro17@gmail.com>
2018-11-25 16:47:04 +0100

    Stratified 10f x-validation working for NB

--- code/evaluation.py
+++ code/evaluation.py
@@ -1,22 +1,21 @@
 # COMPARE COMPUTATIONAL AND PREDICTIVE PERFORMANCE.
 import pandas as pd
-from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import StratifiedKFold, train_test_split
+
 from naive_bayes import NaiveBayes
 
+dataset = pd.read_csv("data/spambase.csv")
+features = list(dataset.drop("spam", axis=1))
+target = "spam"
+x = dataset[features]
+y = dataset[target]
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.10)
 
-class Evaluation:
-    def __init__(self, dataset_name):
-        # Import dataset.
-        self.dataset = pd.read_csv(dataset_name)
-        print("Imported dataset '{}'".format(dataset_name))
-
-    # Run stratified ten-fold cross-validation tests.
-    def stratified_10f_x_validation(self, tr_dataset, tr_feat, target):
-        skf = StratifiedKFold(n_splits=10, shuffle=True)
-        for train, test in skf.split(tr_dataset[tr_feat], tr_dataset[target]):
-            print("%s %s" % (train, test))
-
-
-ev = Evaluation("data/spambase.csv")
 nb = NaiveBayes()
+
+skf = StratifiedKFold(n_splits=10, shuffle=True)
+for train_index, test_index in skf.split(x_train, y_train):
+    nb.train(x_train.values[train_index], y_train.values[train_index])
+    nb.test(x_train.values[test_index], y_train.values[test_index])
+    nb.print_results()
 
